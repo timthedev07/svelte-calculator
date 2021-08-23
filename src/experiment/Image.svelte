c339c77066ba7c1425a3aa425671bc47e9cf0368
Tim <harvard90873@gmail.com>
2021-08-23 12:23:56 +0200

    complete iamge.svelte

---
+++ src/experiment/Image.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const src = "images/favoritetechnologies.jpg";
+</script>
+
+<!-- when the variable and the prop name have the same name
+     we can simplify `prop={prop}` to `{prop}` -->
+<img {src} alt="" />
+
+<style>
+  img {
+    width: 100%;
+  }
+</style>
