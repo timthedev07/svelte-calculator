edc8e4acba83062cf58ff1b971ae5084bd9a20d3
Tim <harvard90873@gmail.com>
2021-08-31 17:05:22 +0200

    more descriptive argument name

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,10 +1,10 @@
 export const operate = (
-  a: number | null,
-  b: number | null,
+  operand0: number | null,
+  operand1: number | null,
   operator: Operator
 ): number => {
-  const num0 = a || 0;
-  const num1 = b || (operator === "/" || operator === "*" ? 1 : 0);
+  const num0 = operand0 || 0;
+  const num1 = operand1 || (operator === "/" || operator === "*" ? 1 : 0);
 
   switch (operator) {
     case "+": {
@@ -17,7 +17,7 @@ export const operate = (
       return num0 * num1;
     }
     case "/": {
-      if (b === 0) {
+      if (operand1 === 0) {
         alert("Invalid 0 division.");
         return 0;
       } else {
