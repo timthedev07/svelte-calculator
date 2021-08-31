2d30f1376dee062934fa2cf44330e7c1e4a5c61d
Tim <harvard90873@gmail.com>
2021-08-31 17:15:03 +0200

    add an interface

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
-type Operator = "+" | "-" | "*" | "/" | "%";
+export type Operator = "+" | "-" | "*" | "/" | "%";
+
+export interface CalculatorState {
+  result: number;
+  currentOperand: number;
+  operator: Operator;
+}
