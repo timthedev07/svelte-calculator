e7e4c4bf9c1192bf8484b5a5b7534da89b37683e
Tim <harvard90873@gmail.com>
2021-09-01 08:58:08 +0200

    a few more tests

--- src/Calculator.test.ts
+++ src/Calculator.test.ts
@@ -2,6 +2,13 @@ import { describe, test, expect } from "@jest/globals";
 import { handleAction } from "./Calculator";
 import { CalculatorState } from "./types";
 
+const initialState = {
+  currOperand: undefined,
+  operator: undefined,
+  prevOperand: undefined,
+  result: undefined,
+} as CalculatorState;
+
 describe("Calculator action handler", () => {
   test("correctly clears everything on AC", () => {
     expect(
@@ -14,11 +21,54 @@ describe("Calculator action handler", () => {
         },
         "AC"
       )
+    ).toEqual(initialState);
+  });
+
+  test("correctly deletes a digit or . on DEL", () => {
+    expect(
+      handleAction(
+        {
+          ...initialState,
+          currOperand: "19239",
+        },
+        "DEL"
+      )
     ).toEqual({
-      currOperand: undefined,
-      operator: undefined,
-      prevOperand: undefined,
-      result: undefined,
-    } as CalculatorState);
+      ...initialState,
+      currOperand: "1923",
+    });
+    expect(
+      handleAction(
+        {
+          ...initialState,
+          currOperand: "192.",
+        },
+        "DEL"
+      )
+    ).toEqual({
+      ...initialState,
+      currOperand: "192",
+    });
+  });
+
+  describe("correctly performs arithmetic operations", () => {
+    test("sums two numbers up", () => {
+      expect(
+        handleAction(
+          {
+            currOperand: "10",
+            operator: "+",
+            prevOperand: "400",
+            result: undefined,
+          },
+          "="
+        )
+      ).toEqual({
+        currOperand: undefined,
+        operator: undefined,
+        prevOperand: "410",
+        result: "410",
+      });
+    });
   });
 });
