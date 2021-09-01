9d5ee1d92ca7dcb577577b405cd8f02b11d28fb9
Tim <harvard90873@gmail.com>
2021-09-01 10:36:27 +0200

    remove result attribute

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 export type Operator = "+" | "-" | "*" | "/" | "%";
 
 export interface CalculatorState {
-  result?: string;
   prevOperand?: string;
   currOperand?: string;
   operator?: Operator;
